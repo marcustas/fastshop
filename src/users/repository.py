b7e3590742627b3f9d733172383a3a274d1eac45
ChernovMykola <4ernovnikola@gmail.com>
2023-12-22 16:51:02 +0100

    Chernov_HW18

--- src/users/repository.py
+++ src/users/repository.py
@@ -1,4 +1,7 @@
-from typing import Optional
+from typing import (
+    Optional,
+    List,
+)
 
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,8 +12,13 @@ from src.common.repository.sqlalchemy import BaseSQLAlchemyRepository
 from src.users.models.pydantic import (
     UserModel,
     UserWithPassword,
+    UserAddressModel,
+    UserAddressDetailModel,
 )
-from src.users.models.sqlalchemy import User
+from src.users.models.sqlalchemy import (
+    User,
+    UserAddress,
+)
 
 
 class UserRepository(BaseSQLAlchemyRepository[User, UserModel]):
@@ -35,3 +43,27 @@ class UserRepository(BaseSQLAlchemyRepository[User, UserModel]):
 
 def get_user_repository(session: AsyncSession = Depends(get_session)) -> UserRepository:
     return UserRepository(session=session)
+
+
+class UserAddressRepository(BaseSQLAlchemyRepository[UserAddress, UserAddressModel]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(model=UserAddress, pydantic_model=UserAddressModel, session=session)
+
+    async def get_user_addresses(self, user_id: int) -> List[UserAddressModel]:
+        stmt = select(self.model).filter_by(user_id=user_id)
+        result_obj = await self.session.execute(stmt)
+        result = result_obj.scalars().all()
+        return [self.pydantic_model.model_validate(instance) for instance in result]
+
+    async def get_address_detail(self, address_id: int, user_id: int) -> Optional[UserAddressDetailModel]:
+        stmt = select(self.model).filter_by(id=address_id, user_id=user_id)
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()
+        if not user:
+            return None
+
+        return UserAddressDetailModel.model_validate(user)
+
+
+def get_users_addresses_repository(session: AsyncSession = Depends(get_session)) -> UserAddressModel:
+    return UserAddressRepository(session=session)
--- src/users/routes.py
+++ src/users/routes.py
@@ -3,7 +3,12 @@ from src.common.routes import BaseCrudPrefixes
 
 class UserManagementRoutesPrefixes:
     user: str = '/user'
+    user_address: list = '/user_address'
 
 
 class UserRoutesPrefixes(BaseCrudPrefixes):
+    ...
+
+
+class UserAddressRoutesPrefixes(BaseCrudPrefixes):
     ...
