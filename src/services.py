33a1ca472a0ad3b58f4b330283d2493196d07652
haym11 <141355160+haym11@users.noreply.github.com>
2023-12-29 21:17:46 +0200

    Add files via upload

---
+++ src/services.py
@@ -0,0 +1,37 @@
+from fastapi import Depends
+
+from src.catalogue.models.database import Product, RecommendedProducts, AdditionalProducts
+from src.catalogue.repository import (
+    ProductRepository,
+    get_product_repository,
+    RecommendedProductsRepository,
+    get_recommended_products_repository,
+    AdditionalProductsRepository,
+    get_additional_products_repository
+)
+from src.common.service import BaseService
+
+
+    class ProductService(BaseService[Product]):
+        def __init__(self, repository: ProductRepository):
+            super().__init__(repository)
+
+
+def get_product_service(repo: ProductRepository = Depends(get_product_repository)) -> ProductService:
+    return ProductService(repository=repo)
+
+class RecommendedProductsService(BaseService[RecommendedProducts]):
+    def __init__(self, repository: RecommendedProductsRepository):
+        super().__init__(repository)
+
+def get_recommended_products_service(repo: RecommendedProductsRepository = Depends(get_recommended_products_repository)) \
+        -> RecommendedProductsService:
+    return RecommendedProductsService(repository=repo)
+
+class AdditionalProductsService(BaseService[AdditionalProducts]):
+    def __init__(self, repository: AdditionalProductsRepository):
+        super().__init__(repository)
+
+def get_additional_products_service(repo: AdditionalProductsRepository = Depends(get_additional_products_repository)) \
+        -> AdditionalProductsService:
+    return AdditionalProductsService(repository=repo)
