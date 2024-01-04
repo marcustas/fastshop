52036fe84dbbc502019a3b1a5b752e8c628eb9f1
Andrea <mateacademy.andrea@gmail.com>
2024-01-04 19:59:56 +0200

    home_work_21

--- src/catalogue/services.py
+++ src/catalogue/services.py
@@ -4,10 +4,12 @@ from elasticsearch.exceptions import ConnectionError
 from fastapi import Depends
 
 from src.base_settings import base_settings
-from src.catalogue.models.database import Product
+from src.catalogue.models.database import Product , Category
 from src.catalogue.repository import (
     ProductRepository,
     get_product_repository,
+    CategoryRepository ,
+    get_category_repository ,
 )
 from src.catalogue.utils import ProductElasticManager
 from src.common.enums import TaskStatus
@@ -45,3 +47,29 @@ class ProductService(BaseService[Product]):
 
 def get_product_service(repo: ProductRepository = Depends(get_product_repository)) -> ProductService:
     return ProductService(repository=repo)
+
+class CategoryService(BaseService[Category]):
+    def __init__(self, repository: CategoryRepository):
+        super().__init__(repository)
+
+    @staticmethod
+    async def search(keyword: str):
+        result = await ProductElasticManager().search_category(keyword=keyword)
+        return result
+
+    async def update_search_index(self, uuid):
+        products = await self.list()
+
+        try:
+            await ProductElasticManager().update_category_index(categories=products)
+        except ConnectionError as exc:
+            await TaskStatusModel(uuid=uuid, status=TaskStatus.ERROR, details=str(exc)).save_to_redis()
+
+        await TaskStatusModel(
+            uuid=uuid,
+            status=TaskStatus.DONE,
+            done_at=datetime.utcnow().strftime(base_settings.date_time_format),
+        ).save_to_redis()
+
+def get_category_service(repo: CategoryRepository = Depends(get_category_repository)) -> CategoryService:
+    return CategoryService(repository=repo)
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from src.admin import register_admin_views
 from src.authentication.views import router as auth_router
 from src.base_settings import base_settings
 from src.catalogue.utils import ProductElasticManager
-from src.catalogue.views import product_router
+from src.catalogue.views import product_router, category_router
 from src.common.databases.postgres import (
     engine,
     init_db,
@@ -36,6 +36,11 @@ def include_routes(application: FastAPI) -> None:
     )
     application.include_router(
         router=product_router,
+        prefix=BaseRoutesPrefixes.catalogue,
+        tags=['Catalogue'],
+    )
+    application.include_router(
+        router=category_router,
         prefix=BaseRoutesPrefixes.catalogue,
         tags=['Catalogue'],
     )
