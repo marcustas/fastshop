08349072df9da19b327f07a9acb7d73cde78dbdd
UlyaM17 <forsoc310@gmail.com>
2024-01-04 09:25:23 -0600

    hw 20

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,8 @@ from sqlmodel import SQLModel
 
 from alembic import context
 
-from src.catalogue.models.database import Product, ProductCategory, ProductDiscount, ProductImage, Category, StockRecord
+from src.catalogue.models.database import (Product, ProductCategory, ProductDiscount, ProductImage, Category,
+                                           StockRecord, RecommendedProducts, AdditionalProducts)
 from src.users.models.database import User, UserAddress
 from src.company.models.database import Company
 
