6d0d9b40e313721cd9df050d38079bd6e4fa47ae
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-01-15 23:03:39 +0200

    env was updated

--- migrations/env.py
+++ migrations/env.py
@@ -2,16 +2,11 @@ import asyncio
 import os
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
-
-from alembic import context
-
-from src.catalogue.models.database import Product, ProductCategory, \
-    ProductDiscount, ProductImage, Category, StockRecord
-from src.company.models.database import Company
 
 postgres_url = os.getenv('POSTGRES__URL')
 
